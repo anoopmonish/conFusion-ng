2c6811f35c5cdf071afd7f0619ee7b7b06c3b96e
Anoop Monish CH <Anoop Monish CH>
2017-10-07 14:32:05 +0530

    HTTP Part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -23,6 +23,7 @@ export class DishdetailComponent implements OnInit {
   next: number;
   commentForm: FormGroup;
   comment: Comment;
+  errMess: string;
 
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
@@ -33,7 +34,8 @@ export class DishdetailComponent implements OnInit {
 	}
 
   ngOnInit() {
-	this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
+	this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds,
+      errmess => this.errMess = <any>errmess);
     this.route.params
       .switchMap((params: Params) => this.dishservice.getDish(+params['id']))
       .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
